42d5ccc0ab83d3cce1cb6533d784ac1e45d060e3
Tareq126 <tareq.12610@gmail.com>
2022-10-20 22:41:22 +0600

    selection and disable system added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('js connected');
+function display(playerList) {
+    console.log(playerList);
+
+    const list = document.getElementById('SelectedPlayerList');
+    list.innerHTML = '';
+
+    for (let i = 0; i < playerList.length; i++) {
+        const name = playerList[i].playerName;
+        console.log(name);
+        const li = document.createElement('li');
+        li.innerHTML = `
+        ${name}`;
+
+        list.appendChild(li);
+    }
+}
+const playerArrayList = [];
+
+function selectPlayer(element) {
+    const playerName = element.parentNode.children[1].innerText;
+
+    const playerObject = {
+        playerName: playerName,
+    }
+    playerArrayList.push(playerObject);
+
+    if (playerArrayList.length <= 5) {
+        document.getElementById('playerCount').innerText = playerArrayList.length;
+        display(playerArrayList);
+    }
+    else {
+        alert('Can not select more than five');
+        return;
+    }
+
+}
